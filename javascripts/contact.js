f12d258b3c8ffd6527d94005bc63e46f917fd4e7
adammcc <ajm845@gmail.com>
2013-12-15 22:15:04 -0500

    contact animation

--- javascripts/contact.js
+++ javascripts/contact.js
@@ -9,24 +9,23 @@
 
 $(document).ready(function() {
     $('#github')
-        .css({"opacity":0})   // Set to 0 as soon as possible – may result in flicker, but it's not hidden for users with no JS (Googlebot for instance!)
-        .delay(200)           // Wait for a bit so the user notices it fade in
-        .animate({"opacity":1});  // Fade it back in. Swap css for animate in legacy browsers if required.
-        console.log('yo')
+        // .css({"opacity":0})        // Set to 0 as soon as possible – may result in flicker, but it's not hidden for users with no JS (Googlebot for instance!)
+        .delay(500)                 // Wait for a bit so the user notices it fade in
+        .animate({"left": 255});  // Fade it back in. Swap css for animate in legacy browsers if required.
 
     $('#linkedin')
-        .css({"opacity":0})   // Set to 0 as soon as possible – may result in flicker, but it's not hidden for users with no JS (Googlebot for instance!)
-        .delay(300)           // Wait for a bit so the user notices it fade in
-        .animate({"opacity":1}); 
+        // .css({"opacity":0})   // Set to 0 as soon as possible – may result in flicker, but it's not hidden for users with no JS (Googlebot for instance!)
+        .delay(400)           // Wait for a bit so the user notices it fade in
+        .animate({"left": 265}); 
 
     $('#twitter')
-        .css({"opacity":0})   // Set to 0 as soon as possible – may result in flicker, but it's not hidden for users with no JS (Googlebot for instance!)
-        .delay(400)           // Wait for a bit so the user notices it fade in
-        .animate({"opacity":1}); 
+        // .css({"opacity":0})   // Set to 0 as soon as possible – may result in flicker, but it's not hidden for users with no JS (Googlebot for instance!)
+        .delay(300)           // Wait for a bit so the user notices it fade in
+        .animate({"left": 275}); 
 
     $('#instagram')
-        .css({"opacity":0})   // Set to 0 as soon as possible – may result in flicker, but it's not hidden for users with no JS (Googlebot for instance!)
-        .delay(500)           // Wait for a bit so the user notices it fade in
-        .animate({"opacity":1}); 
+        // .css({"opacity":0})   // Set to 0 as soon as possible – may result in flicker, but it's not hidden for users with no JS (Googlebot for instance!)
+        .delay(200)           // Wait for a bit so the user notices it fade in
+        .animate({"left": 285}); 
 
 });
