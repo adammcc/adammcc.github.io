148336dfd1159a3eb1937e8522ac6e623b3fdc30
adammcc <ajm845@gmail.com>
2013-12-16 15:58:01 -0500

    add sass add pdf resume remove html resume

--- javascripts/contact.js
+++ javascripts/contact.js
@@ -1,31 +1,22 @@
 
-
-// var container = document.querySelector('#container');
-// var msnry = new Masonry( container, {
-//   // options
-//   columnWidth: 200,
-//   itemSelector: '.icon'
-// });
-
 $(document).ready(function() {
     $('#github')
-        // .css({"opacity":0})        // Set to 0 as soon as possible – may result in flicker, but it's not hidden for users with no JS (Googlebot for instance!)
-        .delay(500)                 // Wait for a bit so the user notices it fade in
-        .animate({"left": 255});  // Fade it back in. Swap css for animate in legacy browsers if required.
+        // .css({"opacity":0})      
+        .delay(500)            
+        .animate({"left": 255});  
 
     $('#linkedin')
-        // .css({"opacity":0})   // Set to 0 as soon as possible – may result in flicker, but it's not hidden for users with no JS (Googlebot for instance!)
-        .delay(400)           // Wait for a bit so the user notices it fade in
+        // .css({"opacity":0})  
+        .delay(400)           
         .animate({"left": 265}); 
 
     $('#twitter')
-        // .css({"opacity":0})   // Set to 0 as soon as possible – may result in flicker, but it's not hidden for users with no JS (Googlebot for instance!)
-        .delay(300)           // Wait for a bit so the user notices it fade in
+        // .css({"opacity":0})  
+        .delay(300)          
         .animate({"left": 275}); 
 
     $('#instagram')
-        // .css({"opacity":0})   // Set to 0 as soon as possible – may result in flicker, but it's not hidden for users with no JS (Googlebot for instance!)
-        .delay(200)           // Wait for a bit so the user notices it fade in
+        // .css({"opacity":0})   
+        .delay(200)           
         .animate({"left": 285}); 
-
 });
