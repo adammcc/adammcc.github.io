4e590e1fa8e5663b450a46e71d0e1bcb78e53765
adammcc <ajm845@gmail.com>
2013-12-16 22:35:40 -0500

    edit contact page

--- javascripts/contact.js
+++ javascripts/contact.js
@@ -3,20 +3,20 @@ $(document).ready(function() {
     $('#github')
         // .css({"opacity":0})      
         .delay(500)            
-        .animate({"left": 255});  
+        .animate({"left": 265});  
 
     $('#linkedin')
         // .css({"opacity":0})  
         .delay(400)           
-        .animate({"left": 265}); 
+        .animate({"left": 275}); 
 
     $('#twitter')
         // .css({"opacity":0})  
         .delay(300)          
-        .animate({"left": 275}); 
+        .animate({"left": 285}); 
 
     $('#instagram')
         // .css({"opacity":0})   
         .delay(200)           
-        .animate({"left": 285}); 
+        .animate({"left": 295}); 
 });
