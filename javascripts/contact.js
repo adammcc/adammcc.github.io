5b52002b71393a65a723084d17502767dee61a35
adammcc <ajm845@gmail.com>
2013-12-17 12:31:38 -0500

    change media query

--- javascripts/contact.js
+++ javascripts/contact.js
@@ -1,22 +1,24 @@
 
 $(document).ready(function() {
-    $('#github')
-        // .css({"opacity":0})      
-        .delay(500)            
-        .animate({"left": 265});  
+    if(screen.width > 480) {
+        $('#github')
+            // .css({"opacity":0})      
+            .delay(500)            
+            .animate({"left": 265});  
 
-    $('#linkedin')
-        // .css({"opacity":0})  
-        .delay(400)           
-        .animate({"left": 275}); 
+        $('#linkedin')
+            // .css({"opacity":0})  
+            .delay(400)           
+            .animate({"left": 275}); 
 
-    $('#twitter')
-        // .css({"opacity":0})  
-        .delay(300)          
-        .animate({"left": 285}); 
+        $('#twitter')
+            // .css({"opacity":0})  
+            .delay(300)          
+            .animate({"left": 285}); 
 
-    $('#instagram')
-        // .css({"opacity":0})   
-        .delay(200)           
-        .animate({"left": 295}); 
+        $('#instagram')
+            // .css({"opacity":0})   
+            .delay(200)           
+            .animate({"left": 295});
+    }
 });
