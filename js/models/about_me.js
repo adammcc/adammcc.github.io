2a9f2f164fb7315553850269de161b67c5d73c2d
adammcc <ajm845@gmail.com>
2014-09-27 19:09:58 -0400

    Updated my 'about me' blurb

--- js/models/about_me.js
+++ js/models/about_me.js
@@ -5,7 +5,7 @@ App.AboutMe = DS.Model.extend({
 var AboutMe = [
  {
    id: 1,
-   blurb: "Hi, I'm Adam. I'm a web developer in New York City. I enjoy learning new things and finding elegant solutions to problems. Before I became a developer, I was a high school math teacher in the Bronx. I currently work in Rails and Javascript, and  I'm interested in any project that presents an interesting challenge, fills a compelling need, and/or calls upon creative thinking."
+   blurb: "I’m a web developer in New York City. I like what I do."
  }
 ];
 
